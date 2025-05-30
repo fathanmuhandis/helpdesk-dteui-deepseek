e7a1bd5e4b00e3986a3179397961f35ec84ef8fd
fathanmuhandis <m.fathan11@ui.ac.id>
2025-05-31 01:28:28 +0700

    change library

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_huggingface import HuggingFaceEmbeddings 
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
